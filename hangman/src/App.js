f2ab3c0b1225d8bf6edff3afb4ee670d6563280b
Joe Fong <kapppa.joe@gmail.com>
2021-10-07 15:49:44 +0100

    made the functionality for category component

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
+
+import { useState } from "react";
+
 import Header from "./header";
 import Category from "./category";
 import Guess from "./guess";
 import Score from "./score";
 import Lives from "./lives";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState("");
+
   return (
     <div className="App">
       <Header id="header" />
-      <Category id="category" />
+      <Category
+        id="category"
+        setWordToGuess={setWordToGuess}
+        wordToGuess={wordToGuess}
+      />
       <Guess id="guess" />
       <Score id="score" />
       <Lives id="lives" />
--- hangman/src/category.js
+++ hangman/src/category.js
@@ -1,15 +1,37 @@
-const Category = () => {
+const Category = (props) => {
+  const { setWordToGuess, wordToGuess } = props;
+
+  const colours = ["blue", "red", "green"];
+  const animals = ["cat", "dog", "lion"];
+
+  const randomIntBelow = (num) => {
+    return Math.floor(Math.random() * num);
+  };
+
+  const handleChange = (e) => {
+    let word = "";
+    if (e.target.value === "animals") {
+      word = animals[randomIntBelow(animals.length)];
+    } else if (e.target.value === "colours") {
+      word = colours[randomIntBelow(colours.length)];
+    }
+
+    setWordToGuess(word);
+  };
+
   return (
     <div id="category">
       <label htmlFor="categories">category</label>
-      <select id="categories">
+      <select id="categories" onChange={handleChange}>
         <option value="animals">Animals</option>
         <option value="colours">Colours</option>
       </select>
       <br />
 
       <label htmlFor="guessedWord">Word</label>
-      <span id="guessedWord">_ _ _</span>
+      <span id="guessedWord">
+        {wordToGuess.split("").map((character) => " _ ")}
+      </span>
     </div>
   );
 };
